21e94c92ed05d3b1557e0e5c8c86cc3e23913bf2
Katie Escoto <katie.escoto@gmail.com>
2020-09-07 22:17:16 -0400

    importing css

--- client/index.js
+++ client/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//import { Provider } from "react-redux";
 import { Router } from "react-router-dom";
-//import history from "./history";
-//import store from "./store";
-//import App from "./app";
+import { Provider } from "react-redux";
+import store from "./store";
+import history from "./history";
+import App from "./app";
 
 // establishes socket connection
 //import "./socket";
@@ -16,10 +16,9 @@ ReactDOM.render(
       <div>Hello World!</div>
     </Router>
   </Provider>,
-  document.getElementById("app")
+  document.getElementById('app')
 );
-import { Provider } from 'react-redux'
-import store from './store'
+
 
 ReactDOM.render(
   <Provider store={store}>
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,58 @@
-const isDev = process.env.NODE_ENV === "development";
+// const isDev = process.env.NODE_ENV === "development";
+
+// module.exports = {
+//   mode: isDev ? "development" : "production",
+//   entry: [
+//     "@babel/polyfill", // enables async-await
+//     "./client/index.js",
+//   ],
+//   output: {
+//     path: __dirname,
+//     filename: "./public/bundle.js",
+//   },
+//   resolve: {
+//     extensions: [".js", ".jsx"],
+//   },
+//   devtool: "source-map",
+//   watchOptions: {
+//     ignored: /node_modules/,
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.jsx?$/,
+//         exclude: /node_modules/,
+//         loader: "babel-loader",
+//       },
+//     ],
+//   },
+// };
 
 module.exports = {
-  mode: isDev ? "development" : "production",
-  entry: [
-    "@babel/polyfill", // enables async-await
-    "./client/index.js",
-  ],
+  entry: './client/index.js',
   output: {
     path: __dirname,
-    filename: "./public/bundle.js",
+    filename: './public/bundle.js'
   },
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-  devtool: "source-map",
-  watchOptions: {
-    ignored: /node_modules/,
-  },
+  devtool: 'source-map',
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['react', 'es2015']
+        }
       },
-    ],
-  },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+
+        ]
+      }
+    ]
+  }
 };
